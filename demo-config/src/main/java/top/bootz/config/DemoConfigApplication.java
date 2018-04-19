c1e9704154728f8f07f30242542188ab17f37c1b
Zhangq <momogoing@163.com>
2018-04-19 19:18:44 +0800

    update

--- demo-config/src/main/java/top/bootz/config/DemoConfigApplication.java
+++ demo-config/src/main/java/top/bootz/config/DemoConfigApplication.java
@@ -11,4 +11,5 @@ public class DemoConfigApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoConfigApplication.class, args);
 	}
+
 }
---
+++ demo-config/src/main/java/top/bootz/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package top.bootz.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().httpBasic().and().csrf().disable();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder()).withUser("admin").password("$2a$10$.6QcZeaa/lDZMiRYQrLDtu1wYYCF2dcO56ygnVMAwfBFSGVqk3Sna")
+				.roles("USER", "ADMIN");
+	}
+
+}
