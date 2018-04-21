8a00d1d660a1c3273084d59ea3b002dbab101215
John <momogoing@163.com>
2018-04-21 22:29:15 +0800

    update

--- demoone/src/main/java/top/bootz/demoone/DemooneApplication.java
+++ demoone/src/main/java/top/bootz/demoone/DemooneApplication.java
@@ -2,8 +2,10 @@ package top.bootz.demoone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DemooneApplication implements WebMvcConfigurer {
 
