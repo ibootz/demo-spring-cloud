16becd7a56e6dc9d2bd4bae766d2e300ffd1c7f1
John <momogoing@163.com>
2018-04-21 16:06:54 +0800

    update

--- demo-zuul/src/main/java/top/bootz/zuul/ZuulApplication.java
+++ demo-zuul/src/main/java/top/bootz/zuul/ZuulApplication.java
@@ -2,8 +2,11 @@ package top.bootz.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.retry.annotation.EnableRetry;
@@ -26,6 +29,13 @@ public class ZuulApplication {
 	@Bean
 	public PatternServiceRouteMapper serviceRouteMapper() {
 		return new PatternServiceRouteMapper("(?<name>^.+)-(?<version>v.+$)", "${version}/${name}");
+	}
+
+	@Bean
+	@RefreshScope
+	@ConfigurationProperties("zuul")
+	public ZuulProperties zuulProperties() {
+		return new ZuulProperties();
 	}
 
 }
