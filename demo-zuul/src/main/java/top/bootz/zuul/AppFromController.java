2f437d2d6aeca6375742d85f927978915b3d3266
John <momogoing@163.com>
2018-04-21 16:01:26 +0800

    update

---
+++ demo-zuul/src/main/java/top/bootz/zuul/AppFromController.java
@@ -0,0 +1,20 @@
+package top.bootz.zuul;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class AppFromController {
+
+	@Value("${app.from:default_name}")
+	private String from;
+
+	@GetMapping(value = "/from")
+	public String getFrom() {
+		return from;
+	}
+
+}
